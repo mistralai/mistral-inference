0e5b4586ae9db53158b4448a53191990e94ad4af
Patrick von Platen <patrick.v.platen@gmail.com>
2024-05-22 16:11:13 +0000

    Change to mistral-inference

---
+++ src/mistral_inference/cache.py
@@ -0,0 +1,224 @@
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
+import torch
+from xformers.ops.fmha.attn_bias import (  # type: ignore
+    AttentionBias,
+    BlockDiagonalCausalMask,
+    BlockDiagonalCausalWithOffsetPaddedKeysMask,
+    BlockDiagonalMask,
+)
+
+
+@dataclass
+class CacheInputMetadata:
+    # rope absolute positions
+    positions: torch.Tensor
+    # where tokens should go in the cache
+    cache_positions: torch.Tensor
+
+    # if prefill, use block diagonal causal mask
+    # else use causal with padded key mask
+    prefill: bool
+    mask: AttentionBias
+    seqlens: List[int]
+
+
+def interleave_list(
+    l1: List[torch.Tensor], l2: List[torch.Tensor]
+) -> List[torch.Tensor]:
+    assert len(l1) == len(l2)
+    return [v for pair in zip(l1, l2) for v in pair]
+
+
+class CacheView:
+    def __init__(
+        self,
+        cache_k: torch.Tensor,
+        cache_v: torch.Tensor,
+        metadata: CacheInputMetadata,
+        kv_seqlens: torch.Tensor,
+    ):
+        self.cache_k = cache_k
+        self.cache_v = cache_v
+        self.kv_seqlens = kv_seqlens
+        self.metadata = metadata
+
+    def update(self, xk: torch.Tensor, xv: torch.Tensor) -> None:
+        """
+        to_cache_mask masks the last [max_seq_len] tokens in each sequence
+        """
+        n_kv_heads, head_dim = self.cache_k.shape[-2:]
+        flat_cache_k = self.cache_k.view(-1, n_kv_heads, head_dim)
+        flat_cache_v = self.cache_v.view(-1, n_kv_heads, head_dim)
+
+        flat_cache_k.index_copy_(0, self.metadata.cache_positions, xk)
+        flat_cache_v.index_copy_(0, self.metadata.cache_positions, xv)
+
+    def interleave_kv(
+        self, xk: torch.Tensor, xv: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        This is a naive implementation and not optimized for speed.
+        """
+        assert xk.ndim == xv.ndim == 3  # (B * T, H, D)
+        assert xk.shape == xv.shape
+
+        if all([s == 0 for s in self.metadata.seqlens]):
+            # No cache to interleave
+            return xk, xv
+
+        # Make it a list of [(T, H, D)]
+        xk: Tuple[torch.Tensor] = torch.split(xk, self.metadata.seqlens)  # type: ignore
+        xv: Tuple[torch.Tensor] = torch.split(xv, self.metadata.seqlens)  # type: ignore
+        assert len(xk) == len(
+            self.kv_seqlens
+        ), f"Batch size is {len(self.kv_seqlens)}, got {len(xk)}"
+
+        # Retrieve cache
+        cache_k = [
+            cache_k[:seq_len] for cache_k, seq_len in zip(self.cache_k, self.kv_seqlens)
+        ]
+        cache_v = [
+            cache_v[:seq_len] for cache_v, seq_len in zip(self.cache_v, self.kv_seqlens)
+        ]
+
+        interleaved_k = interleave_list(cache_k, list(xk))
+        interleaved_v = interleave_list(cache_v, list(xv))
+
+        return torch.cat(interleaved_k, dim=0), torch.cat(interleaved_v, dim=0)
+
+    @property
+    def max_seq_len(self) -> int:
+        return self.cache_k.shape[1]
+
+    @property
+    def key(self) -> torch.Tensor:
+        return self.cache_k[: len(self.kv_seqlens)]
+
+    @property
+    def value(self) -> torch.Tensor:
+        return self.cache_v[: len(self.kv_seqlens)]
+
+    @property
+    def prefill(self) -> bool:
+        return self.metadata.prefill
+
+    @property
+    def mask(self) -> AttentionBias:
+        return self.metadata.mask
+
+
+class BufferCache:
+    """
+    This is an example that implements a buffer cache, allowing for variable length sequences.
+    Allocated cache is rectangular which is wasteful (see PagedAttention for better mechanisms)
+    """
+
+    def __init__(
+        self,
+        n_layers: int,
+        max_batch_size: int,
+        max_seq_len: int,
+        n_kv_heads: int,
+        head_dim: int,
+    ):
+        self.max_seq_len = max_seq_len
+        self.n_kv_heads = n_kv_heads
+        self.head_dim = head_dim
+
+        self.cache_k = torch.empty(
+            (n_layers, max_batch_size, max_seq_len, n_kv_heads, head_dim)
+        )
+        self.cache_v = torch.empty(
+            (n_layers, max_batch_size, max_seq_len, n_kv_heads, head_dim)
+        )
+        # holds the valid length for each batch element in the cache
+        self.kv_seqlens: Optional[torch.Tensor] = None
+
+    def get_view(self, layer_id: int, metadata: CacheInputMetadata) -> CacheView:
+        assert self.kv_seqlens is not None
+        return CacheView(
+            self.cache_k[layer_id], self.cache_v[layer_id], metadata, self.kv_seqlens
+        )
+
+    def reset(self) -> None:
+        self.kv_seqlens = None
+
+    def init_kvseqlens(self, batch_size: int) -> None:
+        self.kv_seqlens = torch.zeros(
+            (batch_size,), device=self.device, dtype=torch.long
+        )
+
+    @property
+    def device(self) -> torch.device:
+        return self.cache_k.device
+
+    def to(self, device: torch.device, dtype: torch.dtype) -> "BufferCache":
+        self.cache_k = self.cache_k.to(device=device, dtype=dtype)
+        self.cache_v = self.cache_v.to(device=device, dtype=dtype)
+
+        return self
+
+    def update_seqlens(self, seqlens: List[int]) -> None:
+        assert self.kv_seqlens is not None
+        self.kv_seqlens += torch.tensor(seqlens, device=self.device, dtype=torch.long)
+
+    def get_input_metadata(self, seqlens: List[int]) -> CacheInputMetadata:
+        """
+        Get metadata about cache positions
+        """
+        if self.kv_seqlens is None:
+            self.init_kvseqlens(len(seqlens))
+
+        assert isinstance(self.kv_seqlens, torch.Tensor)
+        assert len(seqlens) == len(
+            self.kv_seqlens
+        ), f"Batch size is {len(self.kv_seqlens)}, got {len(seqlens)}, did you forget to reset cache?"
+        seqpos = self.kv_seqlens.tolist()
+
+        assert len(seqlens) > 0, seqlens
+        cached_elements = torch.tensor(seqlens, device=self.device, dtype=torch.long)
+
+        positions = torch.cat(
+            [torch.arange(pos, pos + seqlen) for pos, seqlen in zip(seqpos, seqlens)]
+        ).to(device=self.device, dtype=torch.long)
+
+        batch_idx = torch.tensor(
+            sum([[i] * seqlen for i, seqlen in enumerate(seqlens)], []),
+            device=self.device,
+            dtype=torch.long,
+        )
+        cache_positions = positions + batch_idx * self.max_seq_len
+
+        first_prefill = seqpos[0] == 0
+        subsequent_prefill = any(seqlen > 1 for seqlen in seqlens)
+        if first_prefill:
+            assert all([pos == 0 for pos in seqpos]), seqpos
+            mask = BlockDiagonalCausalMask.from_seqlens(seqlens).make_local_attention(
+                self.max_seq_len
+            )
+        elif subsequent_prefill:
+            mask = BlockDiagonalMask.from_seqlens(
+                q_seqlen=seqlens,
+                kv_seqlen=[
+                    s + cached_s.clamp(max=self.max_seq_len).item()
+                    for (s, cached_s) in zip(seqlens, self.kv_seqlens)
+                ],
+            ).make_local_attention_from_bottomright(self.max_seq_len)
+        else:
+            mask = BlockDiagonalCausalWithOffsetPaddedKeysMask.from_seqlens(
+                q_seqlen=seqlens,
+                kv_padding=self.max_seq_len,
+                kv_seqlen=(self.kv_seqlens + cached_elements)
+                .clamp(max=self.max_seq_len)
+                .tolist(),
+            )
+
+        return CacheInputMetadata(
+            positions=positions,
+            cache_positions=cache_positions,
+            prefill=first_prefill or subsequent_prefill,
+            mask=mask,
+            seqlens=seqlens,
+        )
