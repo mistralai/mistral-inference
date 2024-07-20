64d0d704f122f8fbf98c42956be21a67c546f9e1
Patrick von Platen <patrick.v.platen@gmail.com>
2024-07-20 19:47:29 +0200

    Improve

--- src/mistral_inference/main.py
+++ src/mistral_inference/main.py
@@ -12,6 +12,7 @@ from mistral_common.protocol.instruct.messages import AssistantMessage, UserMess
 from mistral_common.protocol.instruct.request import ChatCompletionRequest
 from mistral_common.tokens.tokenizers.base import Tokenizer
 from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from mistral_common.tokens.tokenizers.tekken import Tekkenizer, SpecialTokenPolicy
 from mistral_common.tokens.tokenizers.sentencepiece import is_sentencepiece
 from mistral_common.tokens.tokenizers.tekken import is_tekken
 
@@ -35,6 +36,9 @@ def load_tokenizer(model_path: Path) -> MistralTokenizer:
     ), f"Multiple tokenizers {', '.join(tokenizer)} found in `model_path`, make sure to only have one tokenizer"
 
     mistral_tokenizer = MistralTokenizer.from_file(str(model_path / tokenizer[0]))
+
+    if isinstance(mistral_tokenizer.instruct_tokenizer.tokenizer, Tekkenizer):
+        mistral_tokenizer.instruct_tokenizer.tokenizer.special_token_policy = SpecialTokenPolicy.KEEP
 
     logging.info(f"Loaded tokenizer of type {mistral_tokenizer.instruct_tokenizer.__class__}")
 
